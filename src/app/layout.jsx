927a17d3ee5605b02e74f339f2563813db23cd72
Jenny Riess <jennyriess@gmail.com>
2023-08-06 16:53:54 -1000

    tailwind

---
+++ src/app/layout.jsx
@@ -0,0 +1,33 @@
+import { Providers } from '@/app/providers'
+import { Layout } from '@/components/Layout'
+
+import '@/styles/tailwind.css'
+
+export const metadata = {
+  title: {
+    template: '%s - Jenny Riess',
+    default:
+      'Jenny Riess - Software designer, founder, and amateur astronaut',
+  },
+  description:
+    'I’m Jenny, a software designer and entrepreneur based in Honolulu.',
+  alternates: {
+    types: {
+      'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
+    },
+  },
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <body className="flex h-full bg-zinc-50 dark:bg-black">
+        <Providers>
+          <div className="flex w-full">
+            <Layout>{children}</Layout>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  )
+}
